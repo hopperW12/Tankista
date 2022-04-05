e16f9d841777c8bd1adc89d2a0b772a7e5970078
hopperW12 <wolverinetomas@gmail.com>
2022-04-05 19:22:28 +0200

    update FOV

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -12,13 +12,13 @@ public class FieldOfView : MonoBehaviour
         float fov = 90f;
         Vector3 origin = Vector3.zero;
         int rayCount = 50;
-        float angle = 0f;
+        float angle = 90f;
         float angleIncrease = fov / rayCount;
-        float viewDistance = 5f;
+        float viewDistance = 4f;
 
-        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector3[] vertices = new Vector3[rayCount + 2];
         Vector2[] uv = new Vector2[vertices.Length];
-        int[] tringles = new int[rayCount * 3];
+        int[] triangles = new int[rayCount * 3];
 
         vertices[0] = origin;
 
@@ -29,22 +29,20 @@ public class FieldOfView : MonoBehaviour
             vertices[vertexIndex] = vertex;
 
             if (i > 0) {
-                tringles[triangleIndex + 0] = 0;
-                tringles[triangleIndex + 1] = vertexIndex - 1;
-                tringles[triangleIndex + 2] = vertexIndex;
-
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+                
                 triangleIndex += 3;
             }
 
             vertexIndex++;
             angle -= angleIncrease;
         }
-
+        
         mesh.vertices = vertices;   
         mesh.uv = uv;  
-        mesh.triangles = tringles;
-
-
+        mesh.triangles = triangles;
     }
 
     // Update is called once per frame
@@ -54,7 +52,7 @@ public class FieldOfView : MonoBehaviour
     }
 
     public Vector3 GetVectorFromAngle(float angle) {
-        float angleRed = angle * (Mathf.PI / 180f);
-        return new Vector3(Mathf.Cos(angleRed), Mathf.Sin(angleRed));
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad), 0);
     }
 }
